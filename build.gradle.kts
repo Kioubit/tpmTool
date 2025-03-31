bfa399148576a1d492fee7978b89dcc9bf71b5b5
Kioubit <kioubit@localhost.localdomain>
2025-03-31 03:14:13 +0300

    Remove unused parcelize plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.kotlin.ksp) apply false
 }
